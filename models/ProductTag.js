86cc3d0b47efb3f3efed7e1deeaf5da7b5616c77
mar <rojanomariano@gmail.com>
2023-02-16 06:47:12 -0800

    tags columns

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -19,8 +19,15 @@ ProductTag.init(
         model: 'product',
         key: 'id',
       },
-      
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
     }
+
   },
   {
     sequelize,
