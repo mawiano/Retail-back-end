40a7324ed30552a3a8878a4a6fda2d0db5210a26
mar <rojanomariano@gmail.com>
2023-02-16 06:37:27 -0800

    last product column

--- models/Product.js
+++ models/Product.js
@@ -26,6 +26,14 @@ Product.init(
     validate: {
       isDecimal: true, 
     }
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    validate: {
+      isNumeric: true,
+    }
   {
     sequelize,
     timestamps: false,
