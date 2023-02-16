63398135829ad0c12b9b0d1a48ee5854764ead1f
mar <rojanomariano@gmail.com>
2023-02-16 06:58:33 -0800

    create a new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,15 +32,23 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoriesData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
 });
 
