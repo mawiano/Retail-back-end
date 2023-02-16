39046632474ef80ac0fd05e3792f7d4efee9f5f8
mar <rojanomariano@gmail.com>
2023-02-16 07:15:42 -0800

    creating a new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -38,6 +38,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async (req, res) => {
