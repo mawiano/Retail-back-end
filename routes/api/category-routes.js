36555f55c9801ff1fbb7776f11ce90befa2ad597
mar <rojanomariano@gmail.com>
2023-02-16 06:57:09 -0800

    find one category by its uid=197609(rojan) gid=197609 groups=197609 value

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoriesData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}],
+    });
+    if (!categoriesData) {
+      res.status(404).json({ message: "No category found with that ID"});
+    } 
+    res.status(200).json(categoriesData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
